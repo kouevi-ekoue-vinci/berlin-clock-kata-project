c527dd65baa1ad4d977d394030cd73f09c4f5941
kouevi ekoue <kouevi.ekoue@student.vinci.be>
2024-11-13 20:26:59 +0100

    Add function getFiveHours

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 export class berlinClock {
+    
+    getFiveHours(hours) {
+        let row = '';
+        let litBlock = Math.floor(hours / 5);
 
+        for (let i = 0; i < 4; i++) {
+            if (i < litBlock){
+                row += 'R'; // R -> Pour Red quand c'est allumée
+            } else{
+                row += 'O'; // O -> Pour Off quand c'est éteint
+            }
+        }
+        return row;
+    }
 }
