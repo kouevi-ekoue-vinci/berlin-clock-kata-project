5fe438d0583f66ef61ab51e62746fed1c5a0f9eb
kerim-kaplaner-vinci <kerim.kaplaner@student.vinci.be>
2024-11-14 12:18:43 +0100

    Add function getHours

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ export class berlinClock {
         let litBlock = Math.floor(hours / 5);
 
         for (let i = 0; i < 4; i++) {
-            if (i < litBlock){
+            if (i < litBlock) {
                 row += 'R'; // R -> Pour Red quand c'est allumée
-            } else{
+            } else {
                 row += 'O'; // O -> Pour Off quand c'est éteint
             }
         }
@@ -17,5 +17,18 @@ export class berlinClock {
     getSeconds(seconds) {
         return seconds % 2 === 0 ? 'R' : 'O';
     }
-    
+
+    getHours(hours) {
+        let row = '';
+        let litBlock = hours % 5;
+
+        for (let i = 0; i < 4; i++) {
+            if (i < litBlock) {
+                row += 'R'; // R -> Pour Red quand c'est allumée
+            } else {
+                row += 'O'; // O -> Pour Off quand c'est éteint
+            }
+        }
+        return row;
+    }
 }
